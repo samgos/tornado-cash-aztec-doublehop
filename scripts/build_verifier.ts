ae702044506fd1367b73543d38a177924719f318
samgos <samuel.jj.gosling@gmail.com>
2022-03-07 17:33:10 +0000

    improved verifier file management

--- scripts/build_verifier.ts
+++ scripts/build_verifier.ts
@@ -5,20 +5,24 @@ import util from "util"
 import path from "path"
 
 const inputPath = path.join(__dirname, "..", "artifacts/circuits/")
+const outputPath = path.join(__dirname, "..", "src/zkeys/")
 const execute = util.promisify(exec)
 
 async function buildVerifier() {
   const directory = await fs.promises.opendir(inputPath)
 
+  await execute(`mkdir ${outputPath}`)
+
   for await(const circuit of directory) {
     const name = circuit.name.replace(".r1cs", "")
+    const format = name.replace(/^\w/, (c) => c.toUpperCase())
 
     if(circuit.name.includes(".r1cs")) {
       await execute(`zkutil setup --circuit ${inputPath + circuit.name}`)
-      await execute(`zkutil generate-verifier && mv Verifier.sol src/${name}Verifier.sol`)
+      await execute(`zkutil generate-verifier && mv Verifier.sol src/${format}Verifier.sol`)
       await execute(`zkutil export-keys --circuit ${inputPath + circuit.name}`)
-      await execute(`mv verification_key.json src/zk/${name}_verification_key.json`)
-      await execute(`mv proving_key.json src/zk/${name}_proving_key.json`)
+      await execute(`mv verification_key.json ${outputPath + name}_verification_key.json`)
+      await execute(`mv proving_key.json ${outputPath + name}_proving_key.json`)
     }
   }
 
